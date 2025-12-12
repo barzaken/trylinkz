6f35badfe4bdbdf0fdee52f3f905a270c810ae12
barzaken <bar.zaken@boyar.org.il>
2025-12-12 13:04:48 +0700

    trying inapp again

--- src/components/inAppRedirect.tsx
+++ src/components/inAppRedirect.tsx
@@ -8,15 +8,26 @@ export default function InAppRedirect() {
     const isFB = ua.includes("fbav") || ua.includes("fban") || ua.includes("facebook");
     if (isIG || isFB) {
       const url = window.location.href;
-
+      if (window.location.host.includes("trylinkz.vercel.app")) {
+        const path = window.location.pathname.slice(1)
+        // ANDROID – פתיחה בכרום מחוץ ל-InAppBrowser
+        if (ua.includes("android")) {
+          window.location.href = `intent://${path}#Intent;scheme=https;package=com.android.chrome;end`;
+          return;
+        }
+        // iOS – ניסיון לפתוח מחוץ ל-WebView
+        window.location.href = `x-safari-https://${path}`;
+        window.open(`x-safari-https://${path}`, "_blank");
+        return;
+      }
       // ANDROID – פתיחה בכרום מחוץ ל-InAppBrowser
       if (ua.includes("android")) {
         window.location.href = `intent://${location.host}${location.pathname}#Intent;scheme=https;package=com.android.chrome;end`;
         return;
       }
       // iOS – ניסיון לפתוח מחוץ ל-WebView
-      window.location.href = `x-safari-https://www.website.com`;
-      window.open(`x-safari-https://www.website.com`, "_blank");
+      window.location.href = `x-safari-${window.location.href}`;
+      window.open(`x-safari-${window.location.href}`, "_blank");
     }
   }, []);
 
